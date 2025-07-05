f61b9caf77e18ed8def657c18f0e9ec099c3d7a2
Nikhil's <130584896+Nikhils-G@users.noreply.github.com>
2025-07-06 00:02:17 +0530

    Update embedder.py

--- embeddings/embedder.py
+++ embeddings/embedder.py
@@ -1,1 +1,6 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
 
+vectorizer = TfidfVectorizer()
+
+def get_embeddings(text_chunks):
+    return vectorizer.fit_transform(text_chunks).toarray()
