edf26585f5ea30d3872657a82e99527963f6c69e
Nikhil's <130584896+Nikhils-G@users.noreply.github.com>
2025-07-05 22:14:42 +0530

    Update and rename chrome_store.py to faiss_store.py

---
+++ vector_store/faiss_store.py
@@ -0,0 +1,15 @@
+import faiss
+import numpy as np
+
+class VectorStore:
+    def __init__(self, dim):
+        self.index = faiss.IndexFlatL2(dim)
+        self.chunks = []
+
+    def add_embeddings(self, embeddings, chunks):
+        self.index.add(np.array(embeddings))
+        self.chunks.extend(chunks)
+
+    def search(self, query_embedding, top_k=3):
+        D, I = self.index.search(np.array([query_embedding]), top_k)
+        return [self.chunks[i] for i in I[0]]
